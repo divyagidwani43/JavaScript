2c5023a84c76abff8d28f8451e0958e91a8eef0c
divya gidwani <96631510+divyagidwani43@users.noreply.github.com>
2023-06-14 01:35:09 +0530

    let-var-const

---
+++ 4/script.js
@@ -0,0 +1,9 @@
+//three ways to declare variables let const var
+//var (not preffered) is old way of let and is used in the same way
+//let is used to declare variable whose value we can change later
+//we can also declare empty variable using let
+let age = 12;
+age = 13;
+//const is used to define that cant chnage later in code
+const year = 2023;
+//year=322; //will error
