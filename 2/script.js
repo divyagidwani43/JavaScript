21feb3118f80a175c8d45ef68b03a906f899ff56
divya gidwani <96631510+divyagidwani43@users.noreply.github.com>
2023-06-13 21:44:20 +0530

    variable and variable naming

---
+++ 2/script.js
@@ -0,0 +1,28 @@
+
+let js = 'amazing';                                //variable keyword let
+if (js === 'amazing') alert('javascript is FUN!'); //condition
+console.log(40 + 30 + 4);                          //will print answer to console
+
+console.log("jonas");
+
+let firstName = "dee";
+firstName = "deeee";
+console.log(firstName);
+
+//for naming variables camel case is preffered firstNamePerson has NP caps to read easy
+//variable name only allows num alpha _ $ and not start from num or be keywords or 
+//name is a keyword but doesnt give error but must not be used
+//avoid uppercase starting variable name
+
+let $gi = "ihedl";
+console.log($gi);
+
+let PI = 3.14;
+console.log(PI);
+
+/*Declare variables called 'country', 'continent' and 'population' and 
+assign their values according to your own country (population in millions)
+Log their values to the console*/
+let country = "India";
+let continent = "asia";
+console.log(country, continent);
