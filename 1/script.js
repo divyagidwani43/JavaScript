f865bc522abbe4d02615fae552050f203bcfeea1
divya gidwani <96631510+divyagidwani43@users.noreply.github.com>
2023-06-14 20:56:54 +0530

    basics

--- 1/script.js
+++ 1/script.js
@@ -1,4 +1,10 @@
 let js = 'amazing'; //variable js
-if (js === 'amazing') alert('javascript is FUN!'); //condition
+
 console.log(40 + 30 + 4); //will print answer to console
 //console can only store or print 5 values
+
+alert('hello') //will print hello to console
+
+let a = prompt('enter a number'); //promt will ask user to enter a number and data will be stored in variable named a
+console.log(typeof a); //will show data type of a 
+//the value entered is always stored as string
