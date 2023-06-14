50c8ef1ad5bb2c9199bbe208646706375179ffd9
divya gidwani <96631510+divyagidwani43@users.noreply.github.com>
2023-06-14 23:22:17 +0530

    1.15 if else

---
+++ 1.15 if else/script.js
@@ -0,0 +1,18 @@
+const age = 14;
+let yearsLeft = 18 - age;
+if (age > 18) {
+    console.log("can take driving test");
+}
+else console.log(`not eligible wait ${yearsLeft} years`);
+
+//any variable declared in the if(){} or else{} branckets cant be accessed outside
+let birth = 1998;
+if (birth <= 2000) {
+    let century = 20;
+    console.log(century)
+}
+else {
+    let century = 21; //needs redefining
+    console.log(century);
+}
+//    console.log(century); will error
