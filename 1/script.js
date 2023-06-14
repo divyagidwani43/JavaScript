30bee85089f81bc38d14756e97f366761c89155a
divya gidwani <96631510+divyagidwani43@users.noreply.github.com>
2023-06-14 23:01:28 +0530

    basics

--- 1/script.js
+++ 1/script.js
@@ -8,3 +8,5 @@ alert('hello') //will print hello to console
 let a = prompt('enter a number'); //promt will ask user to enter a number and data will be stored in variable named a
 console.log(typeof a); //will show data type of a 
 //the value entered is always stored as string
+
+//ctrl +/ to comment out code
