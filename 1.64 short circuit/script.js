2b51984b1b747d21cfcd3c3e7961950fa60c1c11
divya gidwani <dg532022@gmail.com>
2023-07-23 22:34:13 +0530

    data structures, modern operators, string

--- 1.64 short circuit/script.js
+++ 1.64 short circuit/script.js
@@ -6,12 +6,16 @@ const restaurant = {
 };
 
 // Use ANY data type, return ANY data type, short-circuiting
+// SHORT CIRCUIT is stoping evaluation and not executing values after that
+
+
+
+
 
 console.log('---- OR ----');
 // SHORT CIRCUITING IN OR ->
-// if the first value is truthy it will be evaluated and returned no checking second value
-// otherwise truthy one will be taken
-// if both are false the second one will be taken
+// it will short circuit when it encounter truthy value and execution will stop
+// if all are false the last one will be taken
 
 console.log(3 || 'Jonas');       //3
 console.log('' || 'Jonas');      // ''= falsy
@@ -19,27 +23,43 @@ console.log(true || 0);          //both true so first one
 console.log(undefined || null);  //both false so second
 
 console.log(undefined || 0 || '' || 'Hello' || 23 || null);
-// the first truthy value only in OR as when js find one true value ans is true 
+// when js find one true value ans is true 
 // and other values are ignored as no matter what they are ans is till true
 
-// restaurant.numGuests = 0;
-// const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
-// console.log(guests1);
+// restaurant.numGuests=0;
+// will cause 10 to return not 0 try any other value the value will be returned
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+// if numguests exist it will return numguest but it doesnt so its undefined ie falsy sp 10
+console.log(guests1);
 
-// const guests2 = restaurant.numGuests || 10;
-// console.log(guests2);
+const guests2 = restaurant.numGuests || 10;
+// does the same but no ternary if numguest has value its true so numguest will return if not then 10
+console.log(guests2);
+
+
+
+
+
+
+
+
 
 // AND &&
-// console.log('---- AND ----');
-// console.log(0 && 'Jonas');
-// console.log(7 && 'Jonas');
+console.log('---- AND ----');
+console.log(0 && 'Jonas');
+// && short circuits and returns the first falsy value it encounters
+console.log(7 && 'Jonas');
+// when all truthy the last value is returned
 
-// console.log('Hello' && 23 && null && 'jonas');
+console.log('Hello' && 23 && null && 'jonas');
+// short circuit at 1st falsy no evaluate others after
 
-// // Practical example
-// if (restaurant.orderPizza) {
-//     restaurant.orderPizza('mushrooms', 'spinach');
-// }
+// Practical example
+if (restaurant.orderPizza) {
+    // will see it above exists
+    restaurant.orderPizza('mushrooms', 'spinach');
+}
 
-// restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
-
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+// same as above if first value is false execution will stop
+// if true last value i.e data given will be returned
--- 1.65 nullish operator/script.js
+++ 1.65 nullish operator/script.js
@@ -5,4 +5,5 @@ console.log(guests);
 
 // Nullish: null and undefined (NOT 0 or '')
 const guestCorrect = restaurant.numGuests ?? 10;
+// will only short circuit if value of restaurant.numGuests is null or undefined not at 0 or any other falsy
 console.log(guestCorrect);
