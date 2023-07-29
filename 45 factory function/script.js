6b9c1048659fb22896f16cc8b27171a5341b479a
divya gidwani <dg532022@gmail.com>
2023-07-29 15:37:25 +0530

    working with arrays

---
+++ 45 factory function/script.js
@@ -0,0 +1,16 @@
+function one(radius) {
+    return {
+        radius,
+        two() {
+            console.log('function 2 without function tag name')
+        }
+    };
+}
+//CALLING MAIN FUNC
+console.log(one(3));
+
+//CALLING THE INSIDE FUNCTION
+console.log(one(7).two);
+
+const a = one(7);
+console.log(a.two);
