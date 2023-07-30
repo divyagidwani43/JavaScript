cdd34909a592c5eb9214faa3c22af1f00eea541a
divya gidwani <dg532022@gmail.com>
2023-07-31 00:07:40 +0530

    a closer look at functions

--- 1.78 first class and higher order/script.js
+++ 1.78 first class and higher order/script.js
@@ -1,4 +1,19 @@
+// FIRST ORDER
+const add = (a, v) => a + v
+console.log(add(2, 1))
 
+console.log(add(2, add(2, 2)))
 
+const count = {
+    value: 23,
+    inc: function () { this.value }
+}
 
-
+// HIGH ORDER
+const counts = function (b) {
+    b = 0;
+    return (function () {
+        console.log(1 + 2);
+    })
+}
+counts(9)
