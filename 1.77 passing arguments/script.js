0f04f6f4a3e13d77cf174ee8da1c5e49e28e7e82
divya gidwani <dg532022@gmail.com>
2023-07-31 00:07:33 +0530

    a closer look at functions

--- 1.77 passing arguments/script.js
+++ 1.77 passing arguments/script.js
@@ -1,32 +1,50 @@
-// How Passing Arguments Works: Values vs. Reference
-const flight = 'LH234';
+const flight = "LH234";
 const jonas = {
-    name: 'Jonas Schmedtmann',
-    passport: 24739479284,
-};
+    name: "jonas",
+    passport: 23344563
+}
 
-const checkIn = function (flightNum, passenger) {
-    flightNum = 'LH999';
-    passenger.name = 'Mr. ' + passenger.name;
+function checkIn(flightNum, passengerDetail) {
+    flightNum = "LP211"
+    passengerDetail.name = "Mr " + passengerDetail.name
 
-    if (passenger.passport === 24739479284) {
-        alert('Checked in');
-    } else {
-        alert('Wrong passport!');
+    console.log("checkIN function called ".padStart(36, "-").padEnd(52, "-"))
+    console.log("passengerDetail.passport = ", passengerDetail.passport)
+
+    if (passengerDetail.passport === 23344563) {
+        console.log("passport OK")
+        alert("passport OK")
     }
-};
+    else {
+        console.log("passport WRONG")
+        alert("passport WRONG")
+    }
+}
 
-// checkIn(flight, jonas);
-// console.log(flight);
-// console.log(jonas);
+// BEFORE func call
+console.log(jonas)
 
-// Is the same as doing...
-// const flightNum = flight;
-// const passenger = jonas;
+// AFTER func call
+checkIn(flight, jonas)
+
+//refer to primitive vs reference
+console.log(flight) //will show not changed flight
+// cz we copied the flight variable value to flightNum and then changed it (i.e it will point to new location where updated value is saved) did not change the original value
+console.log("jonas.passport = ", jonas.passport)
+console.log(jonas)  //will show changed name that was done in function
+// cz the reference types are stored in diff stack and copy made of original points to the same adress as of original
+// change made in copy doesnt create another location or stores diff place but is done is that very location where original points to 
 
 const newPassport = function (person) {
-    person.passport = Math.trunc(Math.random() * 100000000000);
-};
+    console.log("new passport function called ".padStart(39, "-").padEnd(52, "-"))
+    console.log("person.passport input =", person.passport)
+    person.passport = Math.trunc(Math.random() * 10000000);
+    console.log("person.passport updated = ", person.passport)
+}
 
-newPassport(jonas);
-checkIn(flight, jonas);
+newPassport(jonas)
+console.log("jonas.passport = ", jonas.passport)
+checkIn(flight, jonas)
+//will have wrong passport or the updated passport that was done
+// in new passport function 
+console.log(jonas)
