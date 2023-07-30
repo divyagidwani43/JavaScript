728d6360355bb2ec2767db7d8500b8173ed327c4
divya gidwani <dg532022@gmail.com>
2023-07-31 00:21:17 +0530

    a closer look at functions

--- 1.79 function accepting callback/script.js
+++ 1.79 function accepting callback/script.js
@@ -5,10 +5,25 @@ const oneWord = function (str) {
 
 const upperFirstWord = function (str) {
     const [first, ...others] = str.split(' ');
-    console.log(others, ...others)
+    // console.log(others, ...others)
     return [first.toUpperCase(), ...others].join(' ');
 };
 console.log(oneWord("javaScript is hard"))
 console.log(upperFirstWord("javaScript is hard"))
 
 // HIGH - ORDER FUNCTION
+const transform = function (str, fn) {
+    console.log(str)
+    console.log(fn(str))
+    console.log("fn.name = ", fn.name)//will give the original func name fn is copy of
+}
+transform("javaScript is hard", oneWord)
+//function given as argument = CALLBACK FUNC
+transform("javaScript is hard", upperFirstWord)
+//instead of writing thw whole function code of upperFirst word
+// we jusr pass in into func using arguments 
+
+function high5() {
+    console.log("👋".repeat(7))
+}
+document.body.addEventListener("click", high5)
