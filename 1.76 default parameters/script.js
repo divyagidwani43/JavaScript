92fd2403cc7a7b83676d0a3c8aa96ff19cf427e1
divya gidwani <dg532022@gmail.com>
2023-07-31 00:07:27 +0530

    a closer look at functions

--- 1.76 default parameters/script.js
+++ 1.76 default parameters/script.js
@@ -1,28 +1,20 @@
-// Default Parameters
-const bookings = [];
-
-const createBooking = function (
-    flightNum,
-    numPassengers = 1,
-    price = 199 * numPassengers
+const booking = [];
+const func_name = function (
+    para1FlightNum
+    , para2TicketNum = 1 //default value if not given in function
+    , para3price = 199 * para2TicketNum //if value not put it would calculate on its own using para 2  
 ) {
-    // ES5
-    // numPassengers = numPassengers || 1;
-    // price = price || 199;
-
-    const booking = {
-        flightNum,
-        numPassengers,
-        price,
-    };
-    console.log(booking);
-    bookings.push(booking);
-};
-
-createBooking('LH123');
-createBooking('LH123', 2, 800);
-createBooking('LH123', 2);
-createBooking('LH123', 5);
-
-createBooking('LH123', undefined, 1000);
-
+    const boking = {
+        para1FlightNum,
+        para2TicketNum,
+        para3price
+        // we do not need to do para3:para3 enhanced lietrals will name the value para3 on its own as its declared above by that var name
+    }
+    booking.push(boking)
+}
+func_name('233f', 23, 23000)
+func_name()
+func_name("LH111", 3)
+// func_name("LH111",,5) //error as we cant skip a para and it wont use default value
+func_name("LH111", undefined, 5)//unefined will use the default val for para2 while giving argument to para3
+console.log(...booking)
