0f296d1d0bb3dd72f80b9532d6c5aafb6e7008a8
divya gidwani <96631510+divyagidwani43@users.noreply.github.com>
2023-08-10 22:27:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-function disemvowel(str) {
-    const str2 = str.replaceAll('a', '').replaceAll('e', '').replaceAll('i', '').replaceAll('o', '').replaceAll('u', '').replaceAll('A', '').replaceAll('E', '').replaceAll('I', '').replaceAll('O', '').replaceAll('U', '')
-    return str2;
-}
-console.log(disemvowel("This website is for losers LOL!"))
+
