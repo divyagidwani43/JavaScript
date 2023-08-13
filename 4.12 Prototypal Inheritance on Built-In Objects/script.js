cb5084bcd757fb48ee08b496adf00efaf6cb6162
divya gidwani <dg532022@gmail.com>
2023-08-13 20:15:44 +0530

    OOPS

---
+++ 4.12 Prototypal Inheritance on Built-In Objects/script.js
@@ -0,0 +1,42 @@
+const Person = function (firstName, birthYear) {
+    // Instance properties
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+
+    // Never to this!
+    // this.calcAge = function () {
+    //   console.log(2037 - this.birthYear);
+    // };
+};
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+console.log(jonas.__proto__);
+// Object.prototype (top of prototype chain)
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 6, 5, 6, 9, 9]; // new Array === []
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+    return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(x => x + 1);
